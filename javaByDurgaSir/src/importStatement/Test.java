cb408658f18f1c472cc49cb986663864d18ed564
Msivangs <62917298+Msivangs@users.noreply.github.com>
2021-10-13 17:37:57 -0500

    importstatementuse

---
+++ javaByDurgaSir/src/importStatement/Test.java
@@ -0,0 +1,16 @@
+package importStatement;
+
+import java.util.ArrayList;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		//Here for below ArrayList we should import util.ArrayList than only code will compile otherwise we will get compile time error
+		ArrayList l = new ArrayList();
+
+	}
+
+}
