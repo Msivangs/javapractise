766dea4dd9d8e322b4fc36fe52263c1be6df9bf4
Msivangs <62917298+Msivangs@users.noreply.github.com>
2021-10-13 17:10:07 -0500

    sourcefileStructure

---
+++ javaByDurgaSir/src/sourceFileStructure/Ex1.java
@@ -0,0 +1,23 @@
+package sourceFileStructure;
+
+
+//file can contain multiple class files and you can use any class file name as program name
+//If class name contains public you should use that specific class name as program name 
+
+public class Ex1 {
+	
+
+}
+
+class B{
+	
+}
+
+class C{
+	
+	
+}
+
+class D{
+	
+}
---
+++ javaByDurgaSir/src/sourceFileStructure/Example2.java
@@ -0,0 +1,36 @@
+package sourceFileStructure;
+
+class Example1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("this is example1");
+
+	}
+
+}
+
+public  class Example2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is example2");
+	}
+
+}
+
+class Example3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is example3");
+	}
+
+}
+
+//if you want to compile any program there should be main method available 
+class Example4 {
+
+
+}
